b0d8e93b60f57825ead2a6506da7c1cd20610451
atienzam <atienzam@oregonstate.edu>
2020-04-02 02:46:17 -0400

    Project 1

--- basic_stats.py
+++ basic_stats.py
@@ -32,11 +32,3 @@ def basic_stats(person_list):
 
     return calc_mean, calc_med, calc_mode
 
-
-p1 = Person("Kyoungmin", 73)
-p2 = Person("Mercedes", 24)
-p3 = Person("Avanika", 48)
-p4 = Person("Marta", 24)
-
-person_list = [p1, p2, p3, p4]
-print(basic_stats(person_list))  # should print a tuple of three values
